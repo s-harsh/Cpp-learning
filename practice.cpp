706304e657e25d0362a8c9928182b97e1e776e4a
s-harsh <harshvardhansonihv@gmail.com>
2022-12-12 18:18:58 +0530

    Anotehr methoud to solve the problem

--- practice.cpp
+++ practice.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 class Solution {
 public:
-// Check If Two String Arrays are Equivalent
-// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/
+// Problem - Check If Two String Arrays are Equivalent
+    // https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string str1,str2="";
         for(auto i:word1){
@@ -14,8 +14,24 @@ public:
         for(auto j:word2){
             str2.append(j);
         }
-        if(str1==str2)  return true;
-        return false;
+        return str1==str2;
+    }
+
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int i = 0, j = 0; // word pointers
+        int m = 0, n = 0; // char pointers
+        
+        while (i < word1.size() and j < word2.size()) {            
+            if (word1[i][m++] != word2[j][n++])
+                return false;
+            
+            if (m >= word1[i].size())
+                i++, m = 0;
+            
+            if (n >= word2[j].size())
+                j++, n = 0;
+        }
+        return i == word1.size() and j == word2.size();
     }
 };
 
