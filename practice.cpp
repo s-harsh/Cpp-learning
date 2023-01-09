780523f7ad82f8666c6a71e9b41306f734296b9d
s-harsh <harshvardhansonihv@gmail.com>
2023-01-09 10:58:57 +0530

    Binary Tree Preorder Traversal

--- practice.cpp
+++ practice.cpp
@@ -2,6 +2,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution{
 public:
 // Problem 1- Check If Two String Arrays are Equivalent
@@ -116,6 +125,24 @@ public:
             if(s[i]==' ' && --k==0)  return s.substr(0,i);
         }
         return s;
+    }
+
+// Problme 8 - 144. Binary Tree Preorder Traversal
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>preorder;
+        if(root ==NULL) return preorder;
+
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            preorder.push_back(root->val);
+
+            if(root->right !=NULL)  st.push(root->right);
+            if(root->left!=NULL)    st.push(root->left);
+        }
+        return preorder;
     }
 
 int main(){
